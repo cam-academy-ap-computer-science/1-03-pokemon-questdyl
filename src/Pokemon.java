ec8f0ee6fb1d1fe05354e26a24bbe6774518a757
questdyl000 <questdyl000@CAM-LAB-23.bgsd.k12.wa.us>
2019-09-09 07:19:21 -0700

    My assignment

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -2,8 +2,11 @@
 public class Pokemon {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// Pokemon program
+		System.out.println("Raichu welcomes you to the world of Pokemon!");
+		System.out.println("\n(\\__/)");
+		System.out.println("(o^.^)");
+		System.out.println("z(_(\") (\")");
 	}
 
 }
